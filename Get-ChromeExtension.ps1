d06fcf1f78b43a9c8e018ff6fdba85716d59ba1f
Shawn K. Hall <6037167+shawnkhall@users.noreply.github.com>
2017-08-20 22:55:32 -0700

    Update Get-ChromeExtension.ps1
    add extension slug to output

--- Get-ChromeExtension.ps1
+++ Get-ChromeExtension.ps1
@@ -7,6 +7,6 @@ function Get-ChromeExtension {
 
     Get-ChildItem "\\$ComputerName\c$\users\*\appdata\local\Google\Chrome\User Data\Default\Extensions\*\*\manifest.json" -ErrorAction SilentlyContinue | % {
         $_.FullName -match 'users\\(.*?)\\appdata' | Out-Null
-        Get-Content $_.FullName -Raw | ConvertFrom-Json | select @{n='ComputerName';e={$ComputerName}}, @{n='User';e={$Matches[1]}}, name, version
+        $f = $_.FullName; Get-Content $_.FullName -Raw | ConvertFrom-Json | select @{n='ComputerName';e={$ComputerName}}, @{n='User';e={$Matches[1]}}, @{n='Slug';e={(get-item $f).Directory.Parent}}, name, version
     }
 }
